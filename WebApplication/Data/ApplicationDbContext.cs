28e72bb5ad42faab21e1d2550ca75c94cdaba521
Shan21813031 <S.AUK@outlook.com>
2022-04-28 04:52:33 +0100

    Add project files.

---
+++ WebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using WebApplication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)   
+        {
+
+        }
+        public DbSet<Post> Post { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<PostCategory> PostCategories { get; set; } 
+
+    }
+}
---
+++ WebApplication/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }   
+
+
+        public string Description { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public ICollection<PostCategory> postCategories { get; set; } = new HashSet<PostCategory>();
+
+    }
+}
