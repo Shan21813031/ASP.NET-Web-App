ab4eed3efe0f73d75055d89e1bfef0498a2fd59f
Shan21813031 <S.AUK@outlook.com>
2022-05-01 00:57:40 +0100

    Search function included - final revision

--- WebApplication/Data/ApplicationDbContext.cs
+++ WebApplication/Data/ApplicationDbContext.cs
@@ -7,7 +7,14 @@ namespace WebApplication.Data
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
         public DbSet<Post> Post { get; set; }
         public DbSet<Category> Category { get; set; }
         public DbSet<PostCategory> PostCategories { get; set; } 
